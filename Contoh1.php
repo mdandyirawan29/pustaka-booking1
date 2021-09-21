185c8f755746a49c97308baad35b475b1198175b
mdandyirawan29 <muh.dandyirawan@gmail.com>
2021-09-21 18:59:18 +0700

    menambahkan Contoh1.php,Welcome.php,index.html

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Dandy Irawan
+            saya tingal di daerah tanjung priuk
+            olah raga yang saya sukai adalah 
+            Bulutangkis";
+    }
+    
+}
